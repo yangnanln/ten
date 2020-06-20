8d05cd0a7226f239898a8cf5964393b8e2d93cd1
dehui_yangnan <a123456>
2020-06-20 23:24:22 +0800

    新增文章删除 新增 修改 分页查询 接口

---
+++ tensquare_article/src/main/java/com/tensquare/article/config/MybatisPlusConfig.java
@@ -0,0 +1,13 @@
+package com.tensquare.article.config;
+
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
